c7f833a99718f2717c5ea9b6f14b274c958b3183
ZHE HUA <ZHE HUA@LAPTOP-30VO9BI8>
2019-08-12 14:06:26 +0800

    加入專案檔案。

---
+++ WebApplication1/Controllers/DetailController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Domain;
+
+namespace WebApplication1.Controllers
+{
+    public class DetailController : Controller
+    {
+        // GET: Detail
+        public ActionResult Detail(string id)
+        {
+            var entity = new NorthwindEntities();
+
+            var item = entity.Customers.Where(x => x.CustomerID == id).FirstOrDefault();
+            return View(item);
+        }
+        [HttpPost]
+        public ActionResult Detail(Customers viewModle)
+        {
+            if (!this.ModelState.IsValid)
+
+            {
+                return View(viewModle);
+            }
+            var entity = new NorthwindEntities();
+            return this.View("Detail", viewModle);
+        }
+    }
+}
---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Domain;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+
+
+        {          
+                var entity = new NorthwindEntities();
+                var item = entity.Customers.ToList();
+                     
+
+            return View(item);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
