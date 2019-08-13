6e0a6ece174706509008bba73a8a1b9aeb8ac197
ZHE HUA <ZHE HUA@LAPTOP-30VO9BI8>
2019-08-13 15:17:46 +0800

    ok

--- WebApplication1/Controllers/DetailController.cs
+++ WebApplication1/Controllers/DetailController.cs
@@ -1,14 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using WebApplication1.Domain;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
     public class DetailController : Controller
     {
+        private NorthwindEntities DB = new NorthwindEntities();
+
         // GET: Detail
         public ActionResult Edit(string id)
         {
@@ -17,16 +18,31 @@ namespace WebApplication1.Controllers
             var item = entity.Customers.Where(x => x.CustomerID == id).FirstOrDefault();
             return View(item);
         }
+
         [HttpPost]
-        public ActionResult Detail(Customers viewModle)
+        public ActionResult Edit(Customers viewModle)
         {
             if (!this.ModelState.IsValid)
 
             {
                 return View(viewModle);
             }
-            var entity = new NorthwindEntities();
-            return this.View("Detail", viewModle);
+            using (var e = new NorthwindEntities())
+            {
+            };
+            //var result = new ApiResult<string> {
+            //    Result = "更新成功",
+            //    IsSuccess =true
+            //};
+            var result = new ApiResult<string>
+            {
+                ErrorMessage = new List<string>
+                {
+                    //"更新失敗"
+                }
+            };
+
+            return this.Json(result);
         }
     }
 }
---
+++ WebApplication1/Models/ApiResult.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace WebApplication1.Models
+{
+    public class ApiResult<T>
+    {
+        public T Result { get; set; }
+
+        public bool IsSuccess
+        {
+            get
+            {
+                return !this.ErrorMessage.Any();
+            }
+        }
+
+        public List<string> ErrorMessage { get; set; } = new List<string>();  //初始化List
+    }
+}
